9927ade8ac328995e8495f3cfde7ccf5f31e2ecb
aksh <toxiccoder>
2016-11-14 20:49:41 +0530

    searching user done

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :questions, only:[:create, :destroy]
-  resources :users
+  resources :users do
+    collection do
+    get 'search'
+  end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
